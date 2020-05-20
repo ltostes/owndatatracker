5cd854f1e4f4f06a11e4e9248f21d6e0bc794ab5
Lucas Tostes <lucasrmtostes@gmail.com>
2020-05-20 20:26:53 -0300

    added data download route

--- catalog/views.py
+++ catalog/views.py
@@ -1,10 +1,13 @@
 from django.shortcuts import render
+from django.views import View
+from django.http import HttpResponse
 
 from rest_framework import viewsets
 from rest_framework import permissions
 from catalog.serializers import LabelSerializer, EventSerializer
 from catalog.models import Label, Event, ExtraInfo
 
+import pandas as pd
 # Create your views here.
 
 
@@ -29,4 +32,19 @@ class EventViewSet(viewsets.ModelViewSet):
     def get_queryset(self): 
         return Event.objects.filter(user=self.request.user).all()
 
-    
+
+class DataView(View):
+    """
+    Endpoint to collect an CSV with own data
+    """
+
+    def get(self,request): 
+        response = HttpResponse(content_type='text/csv')
+        response['Content-Disposition'] = 'attachment; filename=export.csv'
+
+        queried = [{'Label' : e.label.name, 'Event' : e.name, 'Timestamp' : e.ts} for e in Event.objects.filter(user=self.request.user).all()]
+
+        df = pd.DataFrame(queried)
+        df.to_csv(path_or_buf=response,index=False)
+
+        return response #Event.objects.filter(user=self.request.user).all()
--- djangoowndatatracker/urls.py
+++ djangoowndatatracker/urls.py
@@ -29,4 +29,5 @@ urlpatterns = [
     path('', include(router.urls)),
     path('api-auth/', include('rest_framework.urls', namespace='rest_framework')),
     path('admin/', admin.site.urls),
+    path('data/', views.DataView.as_view(), name='data_view'),
 ]
